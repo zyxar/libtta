e6899c07884c97e3d709525dc58877dee41979a7
Markus Tzoe <chou.marcus@gmail.com>
2023-12-07 21:32:58 +0800

    avx: improve impl

--- filter_avx.h
+++ filter_avx.h
@@ -10,8 +10,7 @@ static __inline void hybrid_filter_dec(TTA_fltst *fs, TTAint32 *in) {
 	TTAint32 *pB = fs->qm;
 	TTAint32 *pM = fs->dx;
 	TTAint32 sum = fs->round;
-	__simd_i256 xa, xb, xm;
-	__m128i xd;
+	__simd_i256 xa, xb, xm, xd;
 
 	xa.i256 = _mm256_load_si256((__m256i*)pA);
 	xb.i256 = _mm256_load_si256((__m256i*)pB);
@@ -25,17 +24,15 @@ static __inline void hybrid_filter_dec(TTA_fltst *fs, TTAint32 *in) {
 		_mm256_store_si256((__m256i*)pB, xb.i256);
 	}
 
-	xd = _mm_add_epi32(_mm_mullo_epi32(xa.i128[0], xb.i128[0]), _mm_mullo_epi32(xa.i128[1], xb.i128[1]));
-	xd = _mm_add_epi32(xd, _mm_unpackhi_epi64(xd, xd));
-	sum += _mm_cvtsi128_si32(xd) + _mm_cvtsi128_si32(_mm_shuffle_epi32(xd, 1));
+	xd.i256 = _mm256_mullo_epi32(xa.i256, xb.i256);
+	xd.i128[0] = _mm_add_epi32(xd.i128[0], xd.i128[1]);
+	xd.i128[0] = _mm_add_epi32(xd.i128[0], _mm_unpackhi_epi64(xd.i128[0], xd.i128[0]));
+	sum += _mm_cvtsi128_si32(xd.i128[0]) + _mm_extract_epi32(xd.i128[0], 1);
 
-	// xd.i256 = _mm256_mullo_epi32(xa.i256, xb.i256);
-	// sum += _mm_cvtsi128_si32(_mm_hadd_epi32(xd.i128[0], xd.i128[1]));
-
-	xm.i128[0] = _mm_or_si128(_mm_srli_si128(xm.i128[0], 4), _mm_slli_si128(xm.i128[1], 12));
-	xa.i128[0] = _mm_or_si128(_mm_srli_si128(xa.i128[0], 4), _mm_slli_si128(xa.i128[1], 12));
+	xm.i256 = _mm256_alignr_epi8(_mm256_permute2x128_si256(xm.i256, xm.i256, _MM_SHUFFLE(2, 0, 0, 1)), xm.i256, 4); // ignore hi
 	xm.i128[1] = _mm_andnot_si128(_mm_setr_epi32(0, 1, 1, 3),
 		_mm_or_si128(_mm_srai_epi32(xa.i128[1], 30), _mm_setr_epi32(1, 2, 2, 4)));
+	xa.i256 = _mm256_alignr_epi8(_mm256_permute2x128_si256(xa.i256, xa.i256, _MM_SHUFFLE(2, 0, 0, 1)), xa.i256, 4); // ignore hi
 
 	_mm_store_si128((__m128i*)pA, xa.i128[0]);
 	_mm256_store_si256((__m256i*)pM, xm.i256);
@@ -53,8 +50,7 @@ static __inline void hybrid_filter_enc(TTA_fltst *fs, TTAint32 *in) {
 	TTAint32 *pB = fs->qm;
 	TTAint32 *pM = fs->dx;
 	TTAint32 sum = fs->round;
-	__simd_i256 xa, xb, xm;
-	__m128i xd;
+	__simd_i256 xa, xb, xm, xd;
 
 	xa.i256 = _mm256_load_si256((__m256i*)pA);
 	xb.i256 = _mm256_load_si256((__m256i*)pB);
@@ -68,17 +64,15 @@ static __inline void hybrid_filter_enc(TTA_fltst *fs, TTAint32 *in) {
 		_mm256_store_si256((__m256i*)pB, xb.i256);
 	}
 
-	xd = _mm_add_epi32(_mm_mullo_epi32(xa.i128[0], xb.i128[0]), _mm_mullo_epi32(xa.i128[1], xb.i128[1]));
-	xd = _mm_add_epi32(xd, _mm_unpackhi_epi64(xd, xd));
-	sum += _mm_cvtsi128_si32(xd) + _mm_cvtsi128_si32(_mm_shuffle_epi32(xd, 1));
+	xd.i256 = _mm256_mullo_epi32(xa.i256, xb.i256);
+	xd.i128[0] = _mm_add_epi32(xd.i128[0], xd.i128[1]);
+	xd.i128[0] = _mm_add_epi32(xd.i128[0], _mm_unpackhi_epi64(xd.i128[0], xd.i128[0]));
+	sum += _mm_cvtsi128_si32(xd.i128[0]) + _mm_extract_epi32(xd.i128[0], 1);
 
-	// xd.i256 = _mm256_mullo_epi32(xa.i256, xb.i256);
-	// sum += _mm_cvtsi128_si32(_mm_hadd_epi32(xd.i128[0], xd.i128[1]));
-
-	xm.i128[0] = _mm_or_si128(_mm_srli_si128(xm.i128[0], 4), _mm_slli_si128(xm.i128[1], 12));
-	xa.i128[0] = _mm_or_si128(_mm_srli_si128(xa.i128[0], 4), _mm_slli_si128(xa.i128[1], 12));
+	xm.i256 = _mm256_alignr_epi8(_mm256_permute2x128_si256(xm.i256, xm.i256, _MM_SHUFFLE(2, 0, 0, 1)), xm.i256, 4); // ignore hi
 	xm.i128[1] = _mm_andnot_si128(_mm_setr_epi32(0, 1, 1, 3),
 		_mm_or_si128(_mm_srai_epi32(xa.i128[1], 30), _mm_setr_epi32(1, 2, 2, 4)));
+	xa.i256 = _mm256_alignr_epi8(_mm256_permute2x128_si256(xa.i256, xa.i256, _MM_SHUFFLE(2, 0, 0, 1)), xa.i256, 4); // ignore hi
 
 	_mm_store_si128((__m128i*)pA, xa.i128[0]);
 	_mm256_store_si256((__m256i*)pM, xm.i256);
